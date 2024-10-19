40ad64ba2a6c58bb4f6acef61fdd7002eb4cbafb
playlogo <playlogo@yahoo.com>
2024-10-19 17:15:10 +0200

    push
    Co-authored-by: Lars <1ars-d@users.noreply.github.com>
    Co-authored-by: Kaiwen <kaiwxn@users.noreply.github.com>

--- database.go
+++ database.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"database/sql"
-	// _ "github.com/go-sql-driver/mysql"
+	"encoding/json"
+	"fmt"
 )
 
 func accessDB() {
 	db, err := sql.Open("mysql",
-		"user:password@tcp(127.0.0.1:3306)")
+		"root:root@tcp(127.0.0.1:3306)")
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 }
 
-func search(db *sql.DB) {
+func insertData(db *sql.DB) {
 
 }
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -9,7 +11,15 @@ import (
 	"os"
 	"text/template"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql" // Import the MySQL driver
 )
+
+type Product struct {
+	Name  string
+	Price int
+	Image string
+}
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
@@ -21,30 +31,94 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, applyTemplate(body))
 }
 
+func initDB() {
+	dsn := "root:root@tcp(localhost:3306)/check24"
+
+	db, err := sql.Open("mysql", dsn)
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS Products (
+	 	Name VARCHAR(255) PRIMARY KEY NOT NULL,
+	 	Price INT NOT NULL,
+	 	Image VARCHAR(255) NOT NULL
+	 )`)
+
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Open("dump.json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var products []Product
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&products)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, product := range products {
+		_, err := db.Exec(
+			"INSERT INTO Products (Name, Price, Image) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE Price=VALUES(Price), Image=VALUES(Image)", product.Name, product.Price, product.Image)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func queryProducts(q string) []Product {
+
+	dsn := "root:root@tcp(localhost:3306)/check24"
+	// /run/mysqld/mysqld.sock
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	rows, _ := db.Query("SELECT Name, Price, Image FROM Products WHERE Name LIKE ? " + "%" + q + "%")
+
+	var products []Product
+
+	// Iterate through the result set
+	for rows.Next() {
+		var product Product
+
+		// Scan the result into the Product struct
+		rows.Scan(&product.Name, &product.Price, &product.Image)
+		fmt.Println(product.Name)
+
+		// Append the product to the slice
+		products = append(products, product)
+	}
+
+	return products
+}
+
 func getSearch(w http.ResponseWriter, r *http.Request) {
 	searchQuery := r.URL.Query().Get("q")
 	fmt.Printf("got /search request with query %s", searchQuery)
 	body, err := loadPage("search")
+	fmt.Println("a")
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	fmt.Println("b")
 
 	products := []Product{}
 
 	if searchQuery != "" {
-		/* !TODO fetch products from DB */
+		products = queryProducts(searchQuery)
 	}
+	fmt.Println("c")
 
-	products = []Product{
-		{
-			Name:  "Test",
-			Price: 4079,
-			Image: "https://image-placeholder.com/images/actual-size/200x200.png",
-		},
-	}
+	fmt.Println("d")
 
 	io.WriteString(w, applySearchTemplate(body, products))
+	fmt.Println("e")
+
 }
 
 func loadPage(name string) (string, error) {
@@ -54,12 +128,6 @@ func loadPage(name string) (string, error) {
 		return "", err
 	}
 	return string(body), nil
-}
-
-type Product struct {
-	Name  string
-	Price int
-	Image string
 }
 
 func applySearchTemplate(input string, products []Product) string {
@@ -79,7 +147,7 @@ func applySearchTemplate(input string, products []Product) string {
 		panic(err)
 	}
 	fmt.Printf("server closed\n")
-	return ""
+	return result.String()
 }
 
 func applyTemplate(input string) string {
@@ -105,7 +173,10 @@ func applyTemplate(input string) string {
 func main() {
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/search", getSearch)
+	fmt.Println("asd")
 
+	initDB()
+	fmt.Println("asd")
 	err := http.ListenAndServe(":3333", nil)
 
 	if errors.Is(err, http.ErrServerClosed) {
@@ -113,6 +184,6 @@ func main() {
 	} else if err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		os.Exit(1)
-	}
 
+	}
 }
